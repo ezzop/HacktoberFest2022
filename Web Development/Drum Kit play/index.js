aee3c7ee5421861245a31158f742b1f7a2749b62
Aman Maurya <amanmaurya112233@gmail.com>
2022-10-06 20:43:42 +0530

    drum play WEB-D added

---
+++ Web Development/Drum Kit play/index.js
@@ -0,0 +1,69 @@
+// alert("holla ");
+var numberDrum = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberDrum; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    var thisButton = this.innerHTML;
+    makeSpund(thisButton);
+    buttonAnimation(thisButton);
+  });
+}
+
+document.addEventListener("keydown", function() {
+  makeSpund(event.key);
+  buttonAnimation(event.key);
+});
+
+
+
+
+function makeSpund(key) {
+  switch (key) {
+    case "w":
+      var audio = new Audio('sounds/crash.mp3');
+      audio.play();
+      break;
+
+    case "a":
+      var audio = new Audio('sounds/kick-bass.mp3');
+      audio.play();
+      break;
+    case "s":
+      var audio = new Audio('sounds/snare.mp3');
+      audio.play();
+      break;
+
+    case "d":
+      var audio = new Audio('sounds/tom-1.mp3');
+      audio.play();
+      break;
+
+    case "j":
+      var audio = new Audio('sounds/tom-2.mp3');
+      audio.play();
+      break;
+
+    case "k":
+      var audio = new Audio('sounds/tom-3.mp3');
+      audio.play();
+      break;
+
+    case "l":
+      var audio = new Audio('sounds/tom-4.mp3');
+      audio.play();
+      break;
+
+    default:
+      console.log(buttoninnerHTML);
+  }
+
+}
+// button animation
+
+function buttonAnimation(currentkey) {
+  var activeButton = document.querySelector("." + currentkey);
+  activeButton.classList.add("pressed");
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  },100);
+}
